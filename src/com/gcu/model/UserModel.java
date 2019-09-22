0f6ea01638500a0d41fc6dc99a296e8d356d3f47
Brady <BBerner@my.gcu.edu>
2019-09-22 16:24:10 -0700

    Updated the user model with first name, last name, and phone number

--- src/com/gcu/model/UserModel.java
+++ src/com/gcu/model/UserModel.java
@@ -1,8 +1,6 @@
 package com.gcu.model;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 /*
 User model that will be used by both the login and registration modules for the time being
@@ -10,6 +8,12 @@ User model that will be used by both the login and registration modules for the 
 public class UserModel {
 
     private int ID;
+    @NotNull(message = "This is a required field")
+    @Size(min = 2, max = 14, message = "Please be sure that your input is more than 2 characters and less than 14")
+    private String firstName;
+    @NotNull(message = "This is a required field")
+    @Size(min = 2, max = 14, message = "Please be sure that your input is more than 2 characters and less than 14")
+    private String lastName;
     @NotNull(message = "This is a required field")
     @Size(min = 2, max = 14, message = "Please be sure that the entered user name is more than 2 characters and less than 14")
     private String username;
@@ -19,6 +23,9 @@ public class UserModel {
     //Had to kill email for time being, threw very nasty errors due to not recognizing email validation constraints
     @NotNull(message = "This is a required field")
     private String email;
+    @NotNull
+    @Pattern(regexp="\\d{3}-\\d{3}-\\d{4}", message = "Please be sure that you've entered your phone number without the country code and with \"-\" between the area codes")
+    private String phoneNumber;
     private int role;
     private int status;
 
@@ -34,10 +41,13 @@ public class UserModel {
 
     //Non-default constructor for use during login
     public UserModel(String username, String password) {
+        firstName = "fname";
+        lastName = "lname";
         this.username = username;
         this.password = password;
         //Email validation ended up not resolving at all, so this ended up being unnecessary (for now)
-        this.email = "test@test.test";
+        email = "test@test.test";
+        phoneNumber = "111-111-1111";
     }
 
     //Non-default constructor for use during registration
@@ -96,5 +106,29 @@ public class UserModel {
 
     public void setStatus(int status) {
         this.status = status;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 }
